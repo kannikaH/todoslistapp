479b92a165bd2b0b9d30164867882c700ffebec4
Kannika Henderson <kanni-h@outlook.com>
2021-07-17 18:45:22 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import TodoList from './components/TodoList';
+
+function App() {
+  return (
+    <div className="todo-app">
+      <h1>My to do list!</h1>
+      <TodoList />
+    </div>
+    
+  );
+}
+
+export default App;
---
+++ src/components/TodoForm.js
@@ -0,0 +1,27 @@
+import React,{useState} from "react";
+
+function TodoForm(props) {
+    const [input,setInput] = useState('');
+    
+    const handleChange = e =>{
+        setInput(e.target.value);
+    };
+    
+      const handleSubmit = e => {
+        e.preventDefault();
+    
+        props.onSubmit({
+          id: Math.floor(Math.random() * 10000),
+          text: input
+        });
+        setInput('');
+      };
+    return(
+       <form className="todo-form" nSubmit={handleSubmit} >
+           <input type='text' placeholder='Add a todo' value={input} name='text' className="todo-input" onChange={handleChange} /> 
+           <button className="todo-button" onClick={handleSubmit}>Add Todo</button>
+       </form>
+    );
+}
+
+export default TodoForm;
